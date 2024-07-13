438495057ef7f5992f355fd90b1f209420a5bb5d
Muhammadnabi <xoliqulovmuhammadnabi842@gmail.com>
2024-07-13 17:33:15 +0500

    masala yechildi va yangi fayl qo'shildi

--- 1.py
+++ 1.py
@@ -5,5 +5,8 @@ f=open("raqamlar.txt","wt+")
 for x in range(1,11):
 	f.write(str(x)+" ")
 f.seek(0,0)
-lis=f.read()
-print(lis)
+lis=f.read().split()
+sum=0
+for y in lis:
+	sum +=int(y)
+print(sum)
